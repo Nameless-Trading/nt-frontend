ed7a787e4d01ab12e1eedebe82630d38912ec819
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-09-12 18:21:32 -0600

    Working websocket testing.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,27 @@
+<script lang="ts">
+    const ws = new WebSocket("ws://localhost:8000/ws")
+
+    ws.onopen = function(event) {
+        console.log("Connected to WebSocket", "system");
+    };
+
+    ws.onmessage = function(event) {
+        console.log("Received: " + event.data, "server");
+    };
+
+    ws.onclose = function(event) {
+        console.log("WebSocket connection closed", "system");
+    };
+    
+    ws.onerror = function(error) {
+        console.log("WebSocket error: " + error, "system");
+    };
+
+    function sendMessage(){
+        ws.send("Hello, world!")
+    }
+
+</script>
+
 <h1>Hello, world!</h1>
+<button onclick={() => sendMessage()}>Send</button>
