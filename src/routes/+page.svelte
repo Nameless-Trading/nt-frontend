5044b8c6898ac7a23d65438c8b1ce50dc5355370
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-09-13 12:36:06 -0600

    Working websocket stuff.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,37 @@
 <script lang="ts">
+    let connected = $state(false);
+    let markets: Record<string, Market> = $state({})
+
+    interface Market{
+        ticker: string
+        bid_price: number
+        bid_quantity: number
+        ask_price: number
+        ask_quantity: number
+    }
+
     const ws = new WebSocket("ws://localhost:8000/ws")
 
     ws.onopen = function(event) {
-        console.log("Connected to WebSocket", "system");
+       connected = true
     };
 
     ws.onmessage = function(event) {
-        console.log("Received: " + event.data, "server");
+        const market: Market = JSON.parse(event.data)
+        console.log(market)
+        const ticker: string = market['ticker']
+        markets = { ...markets, [ticker]: market };
     };
-
-    ws.onclose = function(event) {
-        console.log("WebSocket connection closed", "system");
-    };
-    
-    ws.onerror = function(error) {
-        console.log("WebSocket error: " + error, "system");
-    };
-
-    function sendMessage(){
-        ws.send("Hello, world!")
-    }
 
 </script>
 
-<h1>Hello, world!</h1>
-<button onclick={() => sendMessage()}>Send</button>
+<div>{connected ? 'Connected to websocket' : 'Disconnected from websocket'}</div>
+<div>
+    {#each Object.values(markets) as market}
+    <div>
+        <div>{market.ticker}</div>
+        <div>Bid: {market.bid_price}</div>
+        <div>Ask: {market.ask_price}</div>
+    </div>
+    {/each}
+</div>
